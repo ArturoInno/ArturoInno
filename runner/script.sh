ce253b925ddc58ee024c1261b5d744505b17fa59
ArturoInno <acutanda@inno-it.es>
2026-01-22 14:10:57 +0100

    script.sh created

---
+++ runner/script.sh
@@ -0,0 +1,9 @@
+echo "running python script..."
+echo "Creating test file..."
+touch test.txt
+ls 
+echo "Hello, World!" > test.txt
+echo "adding info to test file..."
+cat test.txt
+rm test.txt
+echo "Test file created and removed."
